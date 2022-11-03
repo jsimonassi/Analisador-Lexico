1b94da7aec70fc0d94bc209d7e49bc5db3c6dba2
João Victor Simonassi <jsimonassi@id.uff.br>
2022-11-02 22:21:22 -0300

    create Analyzer class

---
+++ inputs/full_example.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/*
+Analisador Lexico
+Arquivo de teste
+*/
+
+int main() {
+  int a=8, b=2;
+  int c = a + b;
+
+  if(c > 10) {
+    return 1;
+  }
+
+  return 0;
+}
--- main.py
+++ main.py
@@ -1,32 +1,24 @@
-from constants import STATES, ALL_ELEMENTS, BIN_OPS
+from constants import STATES
 import re
 import utils
+from analyzer import Analyzer
 
 line = 0
 column = 0
-state = STATES.INITIAL
-response_token = []
-token = ""
-numeric_token = ""
-acumula = ""
+
 
 def open_file():
-    """Abre o arquivo de entrada"""
     try:
-        return open("teste.c", "r")
+        return open("inputs/full_example.c", "r")
     except Exception as e:
-        print("Erro ao abrir o arquivo")
+        print("Erro ao abrir o arquivo", e)
         exit(1)
-
-
-def append_error(token):
-    """Adiciona o erro na lista de tokens"""
-    if token != "\n" and token != " ":
-        response_token.append(["Token não reconhecido", token])
 
 
 input_file = open_file()
 if __name__ == '__main__':
+
+    lex_analyzer = Analyzer()
 
     for line_iterator in input_file:
         line = line + 1
@@ -34,108 +26,37 @@ if __name__ == '__main__':
         for cursor in line_iterator:
             column = column + 1
 
+            if lex_analyzer.state == STATES.INITIAL:  # OK
+                if cursor == "/" and line_iterator[column] == "*" and lex_analyzer.state == STATES.INITIAL and lex_analyzer.state != STATES.COMMENT:
+                    lex_analyzer.state = STATES.COMMENT
+                    lex_analyzer.response_token.append(["/*"])
 
-            if state == STATES.INITIAL: #OK
-                if cursor == "/" and line_iterator[column] == "*" and state == STATES.INITIAL and state != STATES.COMMENT:
-                    state = STATES.COMMENT
-                    response_token.append(["/*"])
-
-                if re.search(r"^(#)", line_iterator) and state == STATES.INITIAL and state != STATES.COMMENT:
+                if re.search(r"^(#)", line_iterator) and lex_analyzer.state == STATES.INITIAL and lex_analyzer.state != STATES.COMMENT:
                     break
 
-                if re.search(r"([A-Za-z_])", cursor) and state == STATES.INITIAL and state != STATES.COMMENT:
-                    state = STATES.IDENTIFIER
+                if re.search(r"([A-Za-z_])", cursor) and lex_analyzer.state == STATES.INITIAL and lex_analyzer.state != STATES.COMMENT:
+                    lex_analyzer.state = STATES.IDENTIFIER
 
-                if re.match(r"[0-9]", cursor) and state == STATES.INITIAL and state != STATES.COMMENT:
-                    state = STATES.NUMERIC  # Constante Numérica
-                if re.match(r"[\"]", cursor) and state == STATES.INITIAL and state != STATES.COMMENT:
-                    state = STATES.LITERAL
-                if utils.is_number(cursor) and utils.is_special_caracter(cursor) and state == STATES.INITIAL and state != STATES.COMMENT:
+                if re.match(r"[0-9]", cursor) and lex_analyzer.state == STATES.INITIAL and lex_analyzer.state != STATES.COMMENT:
+                    lex_analyzer.state = STATES.NUMERIC  # Constante Numérica
+                if re.match(r"[\"]", cursor) and lex_analyzer.state == STATES.INITIAL and lex_analyzer.state != STATES.COMMENT:
+                    lex_analyzer.state = STATES.LITERAL
+                if utils.is_number(cursor) and utils.is_special_caracter(cursor) and lex_analyzer.state == STATES.INITIAL and lex_analyzer.state != STATES.COMMENT:
                     if not utils.is_error(cursor):
-                        response_token.append([cursor])
+                        lex_analyzer.response_token.append([cursor])
                     else:
-                        append_error(cursor)
+                        lex_analyzer.append_error(cursor)
 
+            if lex_analyzer.state == STATES.COMMENT:
+                lex_analyzer.check_comment(cursor)
 
+            if lex_analyzer.state == STATES.IDENTIFIER:
+                lex_analyzer.check_identifier(cursor)
 
-            if state == STATES.COMMENT: #OK
-                # TODO: Validar se comentário não ficou com / pendente
-                acumula = acumula + cursor
-                if re.search(r"(\*\/)", acumula):
-                    state = STATES.INITIAL
-                    response_token.append(["*/"])
+            if lex_analyzer.state == STATES.LITERAL:
+                lex_analyzer.check_literal(cursor)
 
+            if lex_analyzer.state == STATES.NUMERIC:
+                lex_analyzer.check_numeric(cursor)
 
-
-            if state == STATES.IDENTIFIER:
-                if re.match(r"([\w])", cursor):
-                    token = token + cursor
-                if utils.is_special_caracter(cursor):
-                    state = STATES.INITIAL
-
-                    if utils.is_reserved_word(token):
-                        response_token.append([token])
-                        if cursor != " ":
-                            if not utils.is_error(token):
-                                response_token.append([token])
-                            else:
-                                append_error(token)
-                        token = ""
-                    else:
-                        response_token.append(["ID", token])
-                        if utils.is_special_caracter(cursor):
-                            """Vai inserir o k como separador """
-                            if cursor != re.match(r"\s", cursor):
-                                if not utils.is_error(cursor):
-                                    response_token.append([cursor])
-                                else:
-                                    append_error(cursor)
-                            state = STATES.INITIAL
-                        token = ""
-
-            if state == STATES.LITERAL: #OK
-                if re.match(r"[%a-zA-z0-9\"\s]", cursor):
-                    token = token + cursor
-                if re.match(r"[\"]", cursor):
-                    lit = re.match(r"[\"]+[%\w\s]+[\"]*", token)
-                    if lit is not None:
-                        response_token.append(["Literal", lit.group()])
-                        token = ""
-                        state = STATES.INITIAL
-
-
-
-            if state == STATES.NUMERIC: #OK
-                if re.match(r"[\w.]", cursor):
-                    numeric_token = numeric_token + cursor
-                if utils.is_number(cursor):
-                    if re.match(r"(^[0-9]*$)", numeric_token):
-                        if re.match(r"(^[0-9]*$)", numeric_token) is not None:
-                            response_token.append(["NUM", numeric_token])
-                            if cursor != " ":
-                                if not utils.is_error(cursor):
-                                    response_token.append([cursor])
-                                else:
-                                    append_error(cursor)
-                            state = STATES.INITIAL
-                            numeric_token = ""
-                    else:
-                        if cursor in ALL_ELEMENTS or re.match(r"\s|\n", cursor) or cursor in BIN_OPS:
-                            """Identifica o token inválido"""
-                            append_error(cursor)
-                            numeric_token = ""
-                            state = STATES.INITIAL
-                else:
-                    if utils.is_number(cursor):
-                        "Armazena token de separadores"
-                        if cursor != " ":
-                            if not utils.is_error(cursor):
-                                response_token.append([cursor])
-                            else:
-                                append_error(cursor)
-                        state = STATES.INITIAL
-
-
-
-
-    print(response_token)
+    print(lex_analyzer.response_token)
